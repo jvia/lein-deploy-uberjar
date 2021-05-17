c2bfdfc2532642c9e833758aae171b2b8f45192a
Jeremiah Via <jeremiah.via@nytimes.com>
2021-05-17 13:18:07 -0700

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.gamayun/deploy-uberjar "0.1.0-SNAPSHOT"
+(defproject io.gamayun/deploy-uberjar "0.1.0"
   :description "A leiningen plugin for deploying uberjars."
   :url "https://github.com/jvia/lein-deploy-uberjar"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
